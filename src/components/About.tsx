d659a03bba1cd888ba73ae102628e7c80a49c671
notbilal22 <muhammadbilaltahir21@gmail.com>
2026-05-24 00:52:02 +0500

    chore: change loading screen initials to BT

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-        I'm Bilal, a CS student and developer focused on building intelligent, scalable systems. My toolkit ranges from the high-performance world of C++ and SQL to modern AI workflows using FastAPI and Machine Learning. I thrive on the challenge of turning complex logic into seamless user experiences. When I'm not in my IDE, you can probably find me analyzing sleep consistency patterns or catching a UCL match.
+        I'm Bilal, a CS student and developer focused on building intelligent, scalable systems. My toolkit ranges from the high-performance world of C++ and SQL to modern AI workflows using FastAPI and Machine Learning. I thrive on the challenge of turning complex logic into seamless user experiences. When I'm not in my IDE, you can probably find me analyzing sleep consistency patterns or catching a UCL football match. I'm extremely grateful to be a quick learner in all the things I do. My communication skills allow me to work really well in a team as well. I'm curios and passionate about learning new and revolutionizing things. 
         </p>
       </div>
     </div>
